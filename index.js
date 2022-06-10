8cf79870a1c24d877807bc1969fd01e9a9293259
Calibry <calibrymark@gmail.com>
2022-06-10 16:38:15 +0300

    'changes 101'

---
+++ index.js
@@ -0,0 +1,14 @@
+const root=document.getElementById("root");
+const menu=["Home","About","contact","products","login"];
+function nav(menu){
+
+    const navBar=document.createElement("nav")
+    for(let item in menu){
+        const a=document.createElement("a");
+        a.innerHTML=menu[item];
+        navBar.appendChild(a);
+
+    }
+    return navBar;
+}
+root.appendChild(nav(menu));
